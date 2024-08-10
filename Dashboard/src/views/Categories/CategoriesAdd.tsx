ae7ed0764b6a466f04495274cc80444e4ff022e2
sampath1102 <sampath.p@convergepoint.com>
2024-08-11 01:50:56 +0530

    updates

--- Dashboard/src/views/Categories/CategoriesAdd.tsx
+++ Dashboard/src/views/Categories/CategoriesAdd.tsx
@@ -13,6 +13,7 @@ import {
 import { TableList } from "../../Components/Table/TableList";
 import React from "react";
 import { useGetCategoriesQuery, useAddCategoriesMutation } from "./Service.mjs";
+import InputNextUI from "../../Components/Common/Input/input";
 
 const CategoriesAdd = () => {
   const { handleSubmit, control, reset } = useForm();
@@ -85,21 +86,53 @@ const CategoriesAdd = () => {
   }, []);
 
   return (
-    <div>
+    <div className="px-2">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col flex-wrap gap-4 border-b pb-2 mb-3">
-          <Chip color="secondary" variant="dot" className="bg-warning-50">
-            Add Categories
+        <div className="flex items-center justify-between border-b pb-3 mt-1.5  mb-3">
+          <Chip
+            classNames={{
+              base: "bg-gradient-to-br  border-small border-white/60 ",
+              content: "drop-shadow shadow-black text-white",
+            }}
+            startContent={
+              <svg
+                width={18}
+                height={18}
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M12 2C6.49 2 2 6.49 2 12C2 17.51 6.49 22 12 22C17.51 22 22 17.51 22 12C22 6.49 17.51 2 12 2ZM16.78 9.7L11.11 15.37C10.97 15.51 10.78 15.59 10.58 15.59C10.38 15.59 10.19 15.51 10.05 15.37L7.22 12.54C6.93 12.25 6.93 11.77 7.22 11.48C7.51 11.19 7.99 11.19 8.28 11.48L10.58 13.78L15.72 8.64C16.01 8.35 16.49 8.35 16.78 8.64C17.07 8.93 17.07 9.4 16.78 9.7Z"
+                  fill="green"
+                />
+              </svg>
+            }
+            variant="faded"
+            color="default"
+          >
+            <p className="font-medium  text-black/70"> Add Categories</p>
           </Chip>
+
+          <div className="text-center">
+            <Button
+              color="primary"
+              type="submit"
+              size="sm"
+              className="w-[90px]"
+            >
+              Submit
+            </Button>
+          </div>
         </div>
         <div>
-          <div className="grid grid-cols-2 gap-4 mb-2">
+          <div className="grid grid-cols-2 gap-4 mb-0.5">
             <Controller
               name="name" // Changed to reflect a text input
               control={control}
               rules={{ required: true }}
               render={({ field }) => (
-                <Input type="text" label="Name" size="lg" {...field} />
+                <InputNextUI type="text" label="Name" size="sm" {...field} />
               )}
             />
             <Controller
@@ -107,15 +140,10 @@ const CategoriesAdd = () => {
               control={control}
               rules={{ required: true }}
               render={({ field }) => (
-                <Input type="text" label="Slug" size="lg" {...field} />
+                <InputNextUI type="text" label="Slug" size="sm" {...field} />
               )}
             />
           </div>
-        </div>
-        <div className="text-center">
-          <Button color="primary" type="submit">
-            Submit
-          </Button>
         </div>
       </form>
       <div>
--- Dashboard/src/views/vendors/Stock/AddStock.tsx
+++ Dashboard/src/views/vendors/Stock/AddStock.tsx
@@ -20,6 +20,7 @@ import {
 } from "./Service.mjs";
 import { TableList } from "../../../Components/Table/TableList";
 import { useGetCategoriesQuery } from "../../Categories/Service.mjs";
+import InputNextUI from "../../../Components/Common/Input/input";
 
 const AddStock = () => {
   const {
@@ -147,20 +148,96 @@ const AddStock = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col flex-wrap gap-4 border-b pb-2 mb-3">
-          <Chip color="secondary" variant="dot" className="bg-warning-50">
-            Add Stock
-          </Chip>
+      <form onSubmit={handleSubmit(onSubmit)} className="px-2">
+        <div className="flex items-center justify-between border-b pb-3 mt-1.5  mb-3">
+          <div>
+            <Chip
+            className="py-4 px-2"
+              classNames={{
+                // "border-1",
+                base: "bg-gradient-to-br  border-small border-white/60 ",
+                content: "drop-shadow shadow-black text-white",
+              }}
+              startContent={
+                <svg
+                  width={18}
+                  height={18}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12 2C6.49 2 2 6.49 2 12C2 17.51 6.49 22 12 22C17.51 22 22 17.51 22 12C22 6.49 17.51 2 12 2ZM16.78 9.7L11.11 15.37C10.97 15.51 10.78 15.59 10.58 15.59C10.38 15.59 10.19 15.51 10.05 15.37L7.22 12.54C6.93 12.25 6.93 11.77 7.22 11.48C7.51 11.19 7.99 11.19 8.28 11.48L10.58 13.78L15.72 8.64C16.01 8.35 16.49 8.35 16.78 8.64C17.07 8.93 17.07 9.4 16.78 9.7Z"
+                    fill="green"
+                  />
+                </svg>
+              }
+              variant="faded"
+              color="default"
+            >
+              <p className="font-medium  text-black/70">Add Stock</p>
+            </Chip>
+          </div>
+          <div className="text-center w-[100px]">
+            <Button color="primary" size="sm" type="submit" className="w-full">
+              Submit
+            </Button>
+          </div>
         </div>
         <div>
-          <div className="grid grid-cols-2 gap-4 mb-2">
+          <div className="grid grid-cols-2 gap-4 mb-0.5">
             <Controller
               name="categoryId" // Changed to reflect a text input
               control={control}
               rules={{ required: true }}
               render={({ field }) => (
-                <Select label="Select Category" {...field}>
+                <Select
+                  classNames={{
+                    label: "group-data-[filled=true]:-translate-y-3",
+                    trigger: [
+                      "bg-transparent",
+                      "border-1",
+                      "text-default-500",
+                      "transition-opacity",
+                      "data-[hover=true]:bg-transparent",
+                      "data-[hover=true]:bg-transparent",
+                      "dark:data-[hover=true]:bg-transparent",
+                      "data-[selectable=true]:focus:bg-transparent",
+                    ],
+                    // listboxWrapper: [
+                    //   "border-1",
+                    //   "text-default-500",
+                    //   "transition-opacity",
+                    //   "data-[hover=true]:text-foreground",
+                    //   "data-[hover=true]:bg-default-100",
+                    //   "dark:data-[hover=true]:bg-default-50",
+                    //   "data-[selectable=true]:focus:bg-default-50",
+                    //   "data-[pressed=true]:opacity-90",
+                    //   "data-[focus-visible=true]:ring-default-500",
+                    // ],
+                  }}
+                  listboxProps={{
+                    itemClasses: {
+                      base: [
+                        "rounded-md",
+                        "text-default-500",
+                        "transition-opacity",
+                        "data-[hover=true]:text-foreground",
+                        "data-[hover=true]:bg-default-100",
+                        "dark:data-[hover=true]:bg-default-50",
+                        "data-[selectable=true]:focus:bg-default-50",
+                        "data-[pressed=true]:opacity-90",
+                        "data-[focus-visible=true]:ring-default-500",
+                        "shadow-none",
+                        // "border-1",
+                      ],
+                    },
+                  }}
+                  label="Select Category"
+                  variant="faded"
+                  size="sm"
+                  {...field}
+                >
                   <SelectItem key={0}>{"Select Category"}</SelectItem>
                   {categoryData?.data?.map((item) => (
                     <SelectItem key={item.id}>{item.name}</SelectItem>
@@ -175,18 +252,19 @@ const AddStock = () => {
               control={control}
               rules={{ required: true }}
               render={({ field }) => (
-                <Input type="text" label="Stock" size="lg" {...field} />
+                <InputNextUI type="text" label="Stock" size="sm" {...field} />
+                // <Input type="text" label="Stock" size="lg" {...field} />
               )}
             />
           </div>
         </div>
-        <div className="text-center">
+        {/* <div className="text-center">
           <Button color="primary" type="submit">
             Submit
           </Button>
-        </div>
+        </div> */}
       </form>
-      <div>
+      <div className="px-2">
         {data && (
           <TableList
             defaultCloumns={defaultCloumns}
