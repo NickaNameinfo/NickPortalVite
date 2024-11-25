927411926d7b234140880cdd59dd6d601adcaea2
ArulKumar8270 <arulkumar8270@gmail.com>
2024-11-26 01:02:05 +0530

    update

--- Frontend/src/configData.tsx
+++ Frontend/src/configData.tsx
@@ -1,5 +1,5 @@
 export const infoData = {
-  // baseApi: "https://nicknameportal.shop/Backend/api",
-  baseApi: "http://localhost:5000/api",
+  baseApi: "https://nicknameportal.shop/Backend/api",
+  // baseApi: "http://localhost:5000/api",
   regex: /[<>:"\/\|?*]/,
 };
