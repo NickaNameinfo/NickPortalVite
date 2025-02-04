f353d85ab75a1344596ee5ff43c586d0d4be16e8
ArulKumar8270 <arulkumar8270@gmail.com>
2025-02-05 01:00:31 +0530

    update

--- Frontend/src/views/pages/Vendor/index.tsx
+++ Frontend/src/views/pages/Vendor/index.tsx
@@ -11,23 +11,28 @@ const Vendore = () => {
 
   return (
     <div>
-      <div className="grid mm:grid-cols-2 ml:grid-cols-2 sm:grid-cols-4  md:grid-cols-4  lg:grid-cols-5  xl:grid-cols-5 2xl:grid-cols-7 3xl:grid-cols-8 gap-2 mt-2">
-        {data?.data?.length > 0 ? (
-          data?.data?.map((item, index) => {
+      {data?.data?.length > 0 ? (
+        <div className="grid mm:grid-cols-2 ml:grid-cols-2 sm:grid-cols-4  md:grid-cols-4  lg:grid-cols-5  xl:grid-cols-5 2xl:grid-cols-7 3xl:grid-cols-8 gap-2 mt-2">
+          {data?.data?.map((item, index) => {
             return item?.status === 1 && <VendorCard item={item} key={index} />;
-          })
-        ) : (
-          <div className="cover h-[68vh] text-gray-400">
-            <h1>
-              Currently, No Vendors Available
-              <br />
-              Join Us Today!
-            </h1>
+          })}
+        </div>
+      ) : (
+        <div className="cover h-[68vh] text-gray-400">
+          <h1>
+            Currently, No Vendors Available
             <br />
-            <h3 className="text-primary">Stay Tuned</h3>
-          </div>
-        )}
-      </div>
+            Join Us Today!
+          </h1>
+          <br />
+          <h3 className="text-primary">
+            Stay Tuned{" "}
+            <a href="https://nicknameinfotech.com/" target="_blank">
+              Nickname infotech
+            </a>
+          </h3>
+        </div>
+      )}
     </div>
   );
 };
